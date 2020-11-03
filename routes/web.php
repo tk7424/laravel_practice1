f9d1707891acd5423f5997332d01a45efbc2f15f
tk7424 <kagataku7424@icloud.com>
2020-11-03 15:31:44 +0900

    Bool一覧を表示、editはまだこれから

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/book', [BookController::class, 'index']);
+Route::get('/edit', [BookController::class, 'edit']);
+
+
