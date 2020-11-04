292d64d4f1b3da61d00e0976587b7b4d096a6c30
tk7424 <kagataku7424@icloud.com>
2020-11-04 14:51:31 +0900

    サンプル完成

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\BookRequest;
 use App\Models\Book;
 
 class BookController extends Controller
 {
+    // 一覧を表示
     public function index()
   {
       // DBよりBookテーブルの値を全て取得
@@ -17,11 +18,51 @@ class BookController extends Controller
       return view('book/index', compact('books'));
   }
 
-    public function edit() {
+    // 編集画面を表示
+    public function edit($id) {
         // DBよりURIパラメータと同じIDを持つBookの情報を取得
         $book = Book::findOrFail($id);
 
         // 取得した値をビュー「book/edit」に渡す
         return view('book/edit', compact('book'));
+    }
+
+
+    // 新規作成画面を表示
+    public function create() {
+        $book = new Book();
+        return view ('book/create', compact('book'));
+    }
+
+    // 新規作成処理
+    public function store(BookRequest $request) {
+        $book = new Book();
+        $book->name = $request->name;
+        $book->price = $request->price;
+        $book->author = $request->author;
+        $book->save();
+
+        return redirect('/book');
+    }
+    
+
+    // 更新処理
+    public function update(BookRequest $request, $id) {
+        $book = Book::findOrFail($id);
+        $book->name = $request->name;
+        $book->price = $request->price;
+        $book->author = $request->author;
+        $book->save();
+
+        return redirect('/book');
+    }
+
+
+    // 削除処理
+    public function destroy($id) {
+        $book = Book::findOrFail($id);
+        $book->delete();
+
+        return redirect('/book');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/book', [BookController::class, 'index']);
-Route::get('/edit', [BookController::class, 'edit']);
-
-
+Route::get('/book/{book}/edit', [BookController::class, 'edit']);
+Route::get('/book/create', [BookController::class, 'create']);
+Route::PUT('/book', [BookController::class, 'store']);
+Route::PUT('/book/{book}', [BookController::class, 'update']);
+Route::delete('/book/{book}', [BookController::class, 'destroy']);
